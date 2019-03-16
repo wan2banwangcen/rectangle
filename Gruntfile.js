04d78802d8cd40b3c6fe5d511d44ccf2c4ea6be0
wan2banwangcen <715586415@qq.com>
2019-03-16 17:27:23 +0800

    rectangel v0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,27 @@ module.exports = function (grunt) {
       reporter:'Spec'
     }
     
+  },
+  htmlmin:{
+    options:{
+      collapseWhitespace:true,
+      preserveLineBreaks: false
+    },
+    files:{
+      src:'./index.html',
+      dest:'dist/index.html'
+    }
+  },
+  cssmin:{
+    'dist/rectangle.css': 'rectangle.css'
+  },
+  uglify:{
+    release:{
+      files:{
+        'dist/rectangle.js': 'rectangle.js',
+        'dist/calc.js': 'calc.js'
+      }
+    }
   }
 });
 
@@ -42,8 +63,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-htmlhint');
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
     grunt.registerTask('unitTest',['mocha']);
+    grunt.registerTask('minify',['htmlmin','cssmin','uglify']);
 
 };

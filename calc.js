095cbfcb4998cc79ce5330406fbb32f73803b6a1
wan2banwangcen <715586415@qq.com>
2019-03-27 19:49:43 +0800

    v1.0-validate

--- calc.js
+++ calc.js
@@ -1,14 +1,31 @@
 function rectangle(){
   return {
     'perimeter':function(w,h){
-            return 2*(Number(w)+Number(h));
-                
+      return 2*(Number(w)+Number(h));          
     },
     'area':function(w,h){
-            return Number(w) * Number(h);
-                
-    }
-      
+      return Number(w) * Number(h);           
+    }   
+  };
+}
+function valid(data){
+  var result = {
+    isOK:false,
+    reason:''
+  };
+  if(data === ''){
+    result.reason = '不能为空';
+    return result;
+  }
+  if(Number(data) < 0){
+    result.reason = '不能为负';
+    return result;
+  }
+  if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data)){
+    result.reason = '必须是数值';
+    return result;
   }
 
+  result.isOK = true;
+  return result;
 }
--- rectangle.js
+++ rectangle.js
@@ -1,11 +1,47 @@
-/* global rectangle: true */
+/* global rectangle,valid: true */
 $(function(){
   var $width = $('#width'),
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
+      $widthValidation = $('#width-validation'),
+      $heightValidation = $('#height-validation'),
       $area = $('#area');
+  $width.focusout(function(){
+    var result = valid($width.val());
+    if(!result.isOK){
+      $widthValidation.html('宽度' + result.reason);
+      $width.select();
+    } else {
+      $widthValidation.html('');
+    }
+  });
+  $height.focusout(function(){
+    var result = valid($height.val());
+    if(!result.isOK){
+      $heightValidation.html('高度' + result.reason);
+      $height.select();
+    } else {
+      $heightValidation.html('');
+    }
+  });
   $btnCal.click(function(){
+    var w = $width.val(),
+        h = $height.val();
+    var widthResult = valid(w);
+    var heightResult = valid(h);
+    if(!widthResult.isOK){
+      $widthValidation.html('宽度' + widthResult.reason);
+      $width.select();
+      return;
+    }
+    if(!heightResult.isOK){
+      $heightValidation.html('高度' + heightResult.reason);
+      $height.select();
+      return;
+    }
+    $widthValidation.html('');
+    $heightValidation.html('');
 
     var r = rectangle();
 

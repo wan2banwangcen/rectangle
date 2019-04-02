16a1ceeefc1e0b48075cdce72fa0c8a57073c6e7
wan2banwangcen <715586415@qq.com>
2019-04-02 14:24:44 +0800

    fead-fe-validation

--- rectangle.js
+++ rectangle.js
@@ -7,24 +7,6 @@ $(function(){
       $widthValidation = $('#width-validation'),
       $heightValidation = $('#height-validation'),
       $area = $('#area');
-  $width.focusout(function(){
-    var result = valid($width.val());
-    if(!result.isOK){
-      $widthValidation.html('宽度' + result.reason);
-      $width.select();
-    } else {
-      $widthValidation.html('');
-    }
-  });
-  $height.focusout(function(){
-    var result = valid($height.val());
-    if(!result.isOK){
-      $heightValidation.html('高度' + result.reason);
-      $height.select();
-    } else {
-      $heightValidation.html('');
-    }
-  });
   $btnCal.click(function(){
     var w = $width.val(),
         h = $height.val();
